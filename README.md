21286710bd9b8e2bfa8a71aa96ec6d2fdcbc3041
Harsh <harshv47@gmail.com>
2020-06-07 15:47:55 +0530

    Everything execpt comments is done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # domain-health
+
+Create a program to queery mx and spf record --read docs
+
+RBL --API/ Scrapper
+
+WHOis public --API
+
+Age of old --API
---
+++ script.py
@@ -0,0 +1,117 @@
+import requests
+import os
+import json
+from datetime import datetime
+
+from dotenv import load_dotenv
+load_dotenv()
+
+domain = str(input())
+
+filePrefix = str(datetime.now())
+
+
+#   MX Part
+mx_api_key = str(os.getenv('MX_API_KEY'))
+
+mx_url = 'https://mxtoolbox.com/api/v1/lookup/mx/' + domain
+
+mx_res = requests.get(url = mx_url, headers={'Authorization': mx_api_key})
+
+mx_data = mx_res.json()
+
+print('The MX info is dumped in ' + filePrefix + '_mx.json')
+
+mx_datas = json.dumps(mx_data)
+
+file_json = open(filePrefix + '_mx.json', 'w')
+file_json.write(mx_datas)
+file_json.close()
+
+#   SPF Part
+
+mx_api_key = str(os.getenv('MX_API_KEY'))
+
+spf_url = 'https://mxtoolbox.com/api/v1/lookup/spf/' + domain
+
+spf_res = requests.get(url = spf_url, headers={'Authorization': mx_api_key})
+
+spf_data = spf_res.json()
+
+print('The SPF info is dumped in ' + filePrefix + '_spf.json')
+
+spf_datas = json.dumps(spf_data)
+
+file_json = open(filePrefix + '_spf.json', 'w')
+file_json.write(spf_datas)
+file_json.close()
+
+
+
+#   WHOis Part
+
+whois_api_key = str(os.getenv('WHOIS_XML_API'))
+
+whois_url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=' + whois_api_key + '&domainName=' + domain + '&outputFormat=JSON'
+
+whois_r =  requests.get(url = whois_url)
+
+whois_data = whois_r.json()
+
+
+if 'registrant' in whois_data['WhoisRecord'].keys():
+
+    if whois_data['WhoisRecord']['registrant']['organization'] == "Whois Privacy Service":
+
+        print('The WHOis Data is not Public, more info in the dumped file ' + filePrefix + '_whois.json')
+
+    else:
+
+        print('The WHOis Data is public, it is available in ' + filePrefix + '_whois.json')
+
+else:
+
+    print('The WHOis Data for this domain may not exist')
+
+
+whois_datas = json.dumps(whois_data)
+
+file_json = open(filePrefix + '_whois.json', 'w')
+file_json.write(whois_datas)
+file_json.close()
+
+
+#   Age of domain:  %b %d %Y %I:%M%p    Jun 12 2013 5:30PM 
+
+date = whois_data['WhoisRecord']['createdDate'][:10]
+
+age = datetime.strptime(date, '%Y-%m-%d')
+
+print(age, datetime.now())
+
+diff = 12*(datetime.now().year - age.year) + (datetime.now().month - age.month)
+
+if diff < 2:
+    print('The domain is quite new, please refrain from sending email campaigns!')
+
+else:
+    print('The domain is sufficiently aged, send away!')
+
+
+#   RBL Check
+
+rbl_api_key = str(os.getenv('RBL_API_KEY'))
+
+rbl_url = 'https://api.hetrixtools.com/v2/' + rbl_api_key + '/blacklist-check/domain/' + domain + '/'
+
+rbl_r = requests.get(url = rbl_url)
+
+rbl_data = rbl_r.json()
+
+rbl_datas = json.dumps(rbl_data)
+
+print('The RBL info is dumped in ' + filePrefix + '_rbl.json')
+
+file_json = open(filePrefix + '_rbl.json', 'w')
+file_json.write(rbl_datas)
+file_json.close()
